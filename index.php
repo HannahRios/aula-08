66542eb2895debfc950f46cb8b9f34c10f911b37
HannahRios <hannah_biatriz@hotmail.com>
2024-10-19 14:45:56 +0000

    criando formulario

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 08</title>
+</head>
+<body>
+    <h1>Introdução a Programção backend</h1>
+
+    <?php
+        echo 'Olá, Hannah! ^.^';
+        echo '<br/>';
+        $ano_nascimento = 1996;
+        $ano_atual = 2024;
+        $idade = $ano_atual - $ano_nascimento;
+
+        echo 'Você possue ' .$idade . ' anos.';
+     ?>
+    <br/>
+    <a href="formulario.php">Ir para o formulario </a>
+</body>
+</html>
